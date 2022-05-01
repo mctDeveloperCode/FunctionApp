d3245d6a3c4a27d6168b02d97f714fb9073ff95e
Michael C. Tiberio <github@tiberio.org>
2022-05-01 14:49:52 -0700

    Adding name to log message

--- src/FunctionApp/SmokeTest.cs
+++ src/FunctionApp/SmokeTest.cs
@@ -17,13 +17,13 @@ namespace MctLearnAzure.FunctionApp
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            log.LogInformation("C# HTTP trigger function processed a request. {name}", string.IsNullOrWhiteSpace(name) ? "<EMPTY>" : name);
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
